d4f5f186627353137dcf445d9ea03245fe4eb162
kytas999 <kytasevichvlad@gmail.com>
2025-07-18 16:26:16 +0300

    fix: email sender name was changed

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -20,7 +20,7 @@ export class EmailService {
 
   async sendEmail(to: string, shareLink: string): Promise<void> {
     const mailOptions = {
-      from: "backup.ai@lijstje.ai",
+      from: `Lijstje.ai <backup@lijstje.ai>`,
       to,
       subject: "Edit verlanglijstje",
       html: `Hallo,<br><br>
