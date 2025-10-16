1d29a567628ddfbf69798b58f3e210040185612b
kytas999 <kytasevichvlad@gmail.com>
2025-10-16 13:29:53 +0300

    feat: new smtp settings

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,41 +1,38 @@
+import { ConfigService } from "@nestjs/config";
+
 import { Injectable } from "@nestjs/common";
-import nodemailer from "nodemailer";
-import { Transporter } from "nodemailer";
+import { Resend } from "resend";
+
+const configService = new ConfigService();
 
 @Injectable()
 export class EmailService {
-  private transporter: Transporter;
+  private resend: Resend;
 
   constructor() {
-    this.transporter = nodemailer.createTransport({
-      host: "h45.mijn.host",
-      port: 465,
-      secure: true,
-      auth: {
-        user: "backup@lijstje.ai",
-        pass: "h^JI5jrEb#Ui01Zg",
-      },
-    });
+    this.resend = new Resend(configService.get<string>("RESEND_API_KEY"));
   }
 
-  async sendEmail(to: string, shareLink: string): Promise<void> {
-    const mailOptions = {
-      from: `Lijstje.ai <backup@lijstje.ai>`,
-      to,
-      subject: "Edit verlanglijstje",
-      html: `Hallo,<br><br>
-Edit jouw verlanglijstje: <a href="${shareLink}">${shareLink}</a><br><br>
-Deel deze met niemand anders mits je wilt dat deze persoon het lijstje inhoudelijk mag aanpassen.<br><br>
-Heb je niet om deze e-mail gevraagd, dan kun je dit bericht negeren. Lijstje.ai zal nooit contact met je opnemen m.b.t. deze e-mail. Pas op voor phishing scams.<br><br>
-Bedankt voor het gebruik van Lijstje.ai!`,
-    };
+  async sendEmail(to: string, editLink: string): Promise<void> {
+    const html = `
+      Hallo,<br><br>
+      Je persoonlijke verlanglijstje is klaar! Open en bewerk ’m 
+      <a href="${editLink}" style="color:#007bff;text-decoration:none;">hier</a>.<br><br>
+      <strong>Belangrijk:</strong> Iedereen met deze link kan producten toevoegen en verwijderen. 
+      Heb je deze mail niet aangevraagd? Negeer ’m dan gerust.<br><br>
+      Bedankt voor het gebruik van Lijstje.ai
+    `;
 
     try {
-      await this.transporter.sendMail(mailOptions);
-      console.log("Email sent successfully!");
+      await this.resend.emails.send({
+        from: "Lijstje.ai <backup@lijstje.ai>",
+        to,
+        subject: "Je verlanglijstje is klaar!",
+        html,
+        replyTo: "lijstje.ai@gmail.com",
+      });
     } catch (error) {
-      console.error("Error sending email:", error);
-      throw new Error("Failed to send email");
+      throw new Error("Failed to send email", error);
     }
   }
 }
