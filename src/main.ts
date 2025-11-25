10d31cfed7b3a2791ff43e44ba00a5208dc61f0b
kytas999 <kytasevichvlad@gmail.com>
2025-11-25 14:29:56 +0200

    update: update cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
       "http://localhost:3000",
       "https://front-gamma-nine.vercel.app",
       "https://lijstje.ai",
+      "https://feest.ai"
     ],
     credentials: true,
   });
